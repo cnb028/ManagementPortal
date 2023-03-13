07e3b8d2018f6d2b65508dfad4e0ff88f71feaa9
Christine_Bettencourt <85318035+cnb028@users.noreply.github.com>
2023-03-13 01:27:21 -0500

    Updated EmployeeContext.cs
    Created EmployeeContext class to inherit the DB Context class

--- Data/EmployeeContext.cs
+++ Data/EmployeeContext.cs
@@ -9,6 +9,40 @@ namespace ManagementPortal.Data
     {
         public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options) { }
 
-        public DbSet<Employee>
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Luke Skywalker",
+                    StartDate = new DateTime(2010, 6 , 15),
+                    Title = "Human Resources Manager",
+                    PayRate = 30,
+                    Hours = 40,
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Leia Organa",
+                    StartDate = new DateTime(2007, 12, 17),
+                    Title = "Junior Accountant",
+                    PayRate = 30,
+                    Hours = 40,
+                },
+                new Employee
+                {
+                    Id = 3,
+                    Name = "Ezra Bridger",
+                    StartDate = new DateTime(2007, 12, 17),
+                    Title = "Junior Accountant",
+                    PayRate = 30,
+                    Hours = 40,
+                }
+            );
+        }
     }
 }
